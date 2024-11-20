19448db46c7a6b202708132013460f1ab06fb739
YarmolenkoD <yarmolenko.dany@gmail.com>
2024-11-20 13:40:56 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,16 @@ import react from '@vitejs/plugin-react'
 
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+import path from 'path'
+
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['src/__tests__/utils/setup.ts']
+  },
+  build: {
+    outDir: path.join(__dirname, "docs"),
   },
 })
